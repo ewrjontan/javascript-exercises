d00de79d50861a9cb2a18bc8ff54bb66e0b0cbde
Jonathan <jonscardenas@gmail.com>
2019-08-17 12:31:52 -0500

    changes to repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
-}
-
-module.exports = repeatString
+const repeatString = function(strInput, repetitions){
+    console.log("hello");
+    
+    let myString = "";
+    
+    if (repetitions === -1){
+      console.log("ERROR");
+      return "ERROR";
+    }else{
+      for (let i=0; i <repetitions; i++){
+        myString += strInput;  
+      }
+    };
+    
+    console.log("final: " + myString);
+    return myString;
+  }
+  
+  module.exports = repeatString
