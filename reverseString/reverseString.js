c499ec74839a8362744b850b251b7c51499ac170
Jonathan <jonscardenas@gmail.com>
2019-08-17 19:53:06 -0500

    changes to reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
-}
-
-module.exports = reverseString
+const reverseString = function(input) {
+    strAsArr = input.split("");
+    
+    arrReverse = strAsArr.reverse();
+    
+    newStr = arrReverse.join(""); 
+    console.log(newStr);
+    
+    return newStr;
+  }
+  
+  //reverseString("hello");
+  //reverseString('123! abc!');
+  module.exports = reverseString
