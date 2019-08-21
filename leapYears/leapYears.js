eaaf3a462aad567d126f6f4eb9fc39c028111d46
Jonathan <jonscardenas@gmail.com>
2019-08-21 15:13:43 -0500

    changes to leapyears main

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,17 +1,14 @@
 const leapYears = function(year) {
     console.log(year%400);
     
-    if (year%400 == false && year%100 == false){
-      console.log(year + " is divisible by 400 and 100");
+    if (year%400 == false && year%100 == false && year&4 == false){
+      console.log(year + " is a leap year");
       return true;
-    }else if (year%4 == false){
-      console.log(year + " is divisible by 4");
-      return true
     }else{
       return false;
     }
   }
   
   
-  //leapYears(1984);
+  //leapYears(700);
   module.exports = leapYears
