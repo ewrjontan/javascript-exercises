15b03d762057c6a133e5efdc6cbf29b671027e5c
Jonathan <jonscardenas@gmail.com>
2019-09-24 12:39:25 -0500

    modified js and js spec

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  let converted = ((input-32) * (5/9));
+  let output = Math.round(converted * 10 ) / 10;
+  console.log(output);
+  return output;
 }
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let converted = ((input * (9/5)) + 32);
+  let output = Math.round(converted * 10 ) / 10;
+  console.log(output);
+  return output;  
 }
+
 
 module.exports = {
   ftoc,
